ea502371cb5b94233c50649a59f5c1200b5761a0
MichelleH013 <58307308+MichelleH013@users.noreply.github.com>
2019-11-28 19:36:01 +0100

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -14,24 +14,24 @@ public class Anagram {
 		wort1=wort1.toLowerCase();
 		wort2=wort2.toLowerCase();
 		String copy2 = new String(wort2);
-	    if (laenge1!=laenge2){
-            System.out.println(wort1+" und "+ wort2+" sind Kein Anagram!");    }
-        else{
-        	while(s<laenge1){
-                c=wort1.charAt(s);
-                if (copy2.indexOf(c) != -1) {
-                	z+=1;
-                	copy2 = copy2.substring(0, copy2.indexOf(c)) + copy2.substring(copy2.indexOf(c)+1);
+	   	if (laenge1!=laenge2){
+            		System.out.println(wort1+" und "+ wort2+" sind Kein Anagram!");    }
+        	else{
+			while(s<laenge1){
+				c=wort1.charAt(s);
+				if (copy2.indexOf(c) != -1) {
+					z+=1;
+					copy2 = copy2.substring(0, copy2.indexOf(c)) + copy2.substring(copy2.indexOf(c)+1);
 				} else {
 					System.out.println(wort1+" und "+ wort2+" sind kein Anagram!");
-                    break;
+				    	break;
 				}
-                s++;
-            }
-        	if (z == laenge1) {
-        		System.out.println(wort1+" und "+ wort2+" sind ein Anagram");
+				s++;
+		    	}
+			if (z == laenge1) {
+				System.out.println(wort1+" und "+ wort2+" sind ein Anagram");
 			}
-    	}      
+		}      
 		sc.close();			
 	}
 }
