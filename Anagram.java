71ce0d0fe1e255bb6cdcc417085c88e7a8160336
MichelleH013 <58307308+MichelleH013@users.noreply.github.com>
2019-11-28 19:46:10 +0100

    Update Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -8,15 +8,14 @@ public class Anagram {
 	String wort2=sc.nextLine();
 	wort1=wort1.toLowerCase();	
 	wort2=wort2.toLowerCase();
-	   char[] c1= wort2.toCharArray();
-	   char[] c2 = wort1.toCharArray();
-	   Arrays.sort(c1);
-	   Arrays.sort(c2);
-	   	if(Arrays.equals( c1, c2 )){		   
-		   System.out.println(wort1+" und "+ wort2+" sind ein Anagram"); 
-	   			}
-	   	else{
-		   System.out.println(wort1+" und "+ wort2+" sind kein Anagram");
-	   		}
-	   sc.close();	   //Arbeitszeit ca. 5-10 min, wie konnte ich da nicht draufkommen????
+	char[] c1= wort2.toCharArray();		//in Array umwandeln
+	char[] c2 = wort1.toCharArray();
+	Arrays.sort(c1);		//alphabetisch sortieren
+	Arrays.sort(c2);
+	if(Arrays.equals( c1, c2 )) {		   //wenn Array 1 = Array 2:
+		System.out.println(wort1+" und "+ wort2+" sind ein Anagram"); 
+	} else {
+		System.out.println(wort1+" und "+ wort2+" sind kein Anagram");
+	}
+	sc.close();	  
 }}
