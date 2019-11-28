170915b618a521937de1c8d15770a3d7b0d1b57f
MichelleH013 <58307308+MichelleH013@users.noreply.github.com>
2019-11-28 19:14:33 +0100

    Update and rename AnagramA.java to Anagram.java

--- Anagram.java
+++ Anagram.java
@@ -1,5 +1,5 @@
 import java.util.*;
-public class AnagramA {
+public class Anagram {
 	public static void main (String args[]){
 		Scanner sc =new Scanner(System.in);
 		System.out.println("Geben Sie ihr Wort ein: ");
